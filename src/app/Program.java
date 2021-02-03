e299b32d4e7ac877f6545267d1ca71f1bc4731e4
Renato Franco <renatofr95@gmail.com>
2021-02-03 10:48:50 -0300

    Classe Seller

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,6 +1,9 @@
 package app;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
 
@@ -8,7 +11,9 @@ public class Program {
 
         Department obj = new Department(1, "Books");
 
-        System.out.println(obj);
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
+
+        System.out.println(seller);
 
     }
 
